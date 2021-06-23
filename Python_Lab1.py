16869d6b47279dc657931432042849f545fa8502
nikkiwojo <85896009+nikkiwojo@users.noreply.github.com>
2021-06-23 08:34:12 -0400

    Add files via upload

---
+++ Python_Lab1.py
@@ -0,0 +1,37 @@
+# Exercise 1
+# Complete
+
+# Exercise 2
+word = True
+print(type(word))
+
+number = 42
+print(type(number))
+
+other_word = False
+print(type(other_word))
+
+other_number = 1.5
+print(type(other_number))
+
+phrase = "Python is fun!"
+print(type(phrase))
+
+other_phrase = "3.5"
+print(type(other_phrase))
+
+# Exercise 3
+x = 10
+y = 20.5
+z = 100
+
+sum = x + y
+diff = z - y
+product = y * x
+quotient = y / z
+floor_div = z // y
+mod = z % y
+exp = x ** 3
+
+print(sum, diff, product, quotient, floor_div, mod, exp)
+

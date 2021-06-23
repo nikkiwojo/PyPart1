2b5a6cfc19a8acd0c5e5658e6eb4b9f52ba6bdf7
nikkiwojo <85896009+nikkiwojo@users.noreply.github.com>
2021-06-23 11:05:38 -0400

    Add files via upload

---
+++ Nikki_Wojo.py
@@ -0,0 +1,7 @@
+first_name = "Nikki "
+# print(first_name * 500)
+
+last_name = "Wojo"
+full_name = first_name + last_name
+
+print(full_name)
---
+++ first_name_500_times.py
@@ -0,0 +1,3 @@
+first_name = "Nikki "
+print(first_name * 500)
+
